939090d5f574063864443d86206ea7329befcb68
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-02-16 15:51:03 +0400

    add simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const sum = (a: number, b: number) => a + b;
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200);
+  res.end('My first server!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
