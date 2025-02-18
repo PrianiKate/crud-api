9755b7e10afdd6b46a0753e2b7a0fba5d33dfd9f
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-02-18 18:58:26 +0400

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import http from 'http';
+import dotenv from 'dotenv';
 
-const PORT = 3000;
+const MAIN_ROUTE = '/api/users';
+dotenv.config();
 
 const server = http.createServer(function (req, res) {
+  console.log(req.method, req.url);
   res.writeHead(200);
   res.end('My first server!');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
